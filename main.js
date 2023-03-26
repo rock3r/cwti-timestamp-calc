93abe59f22cd337b89f3ce24b7e259896835d37a
rock3r <rock3r@users.noreply.github.com>
2023-03-26 18:37:00 +0200

    Initial thing

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-//
-// Place any custom JS here
-//
+const DateTime = luxon.DateTime;
+
+const generateButton = document.getElementById('generateButton');
+const datePicker = document.getElementById('datePicker');
+const nextWednesday = getNextWednesday().toJSDate();
+
+console.log('Next wednesday is:', nextWednesday);
+generateButton.value = nextWednesday;
+
+function getNextWednesday() {
+  const now = DateTime.local();
+  console.log('Now is:', now);
+  if (now.weekday == 3) return now;
+  if (now.weekday < 3) return now.set({ weekday: 3 });
+  return now.set({ weekday: 3, weekNumber: now.weekNumber + 1 });
+}
